5f768e1ff7fb4d3dc3dce117885f29ab6f9f49dd
Brian Paskin <bpaskin@us.ibm.com>
2026-01-25 13:11:17 -0500

    primi passi

---
+++ src/main/java/com/ibm/example/CryptoType.java
@@ -0,0 +1,51 @@
+package com.ibm.example;
+
+import java.nio.charset.StandardCharsets;
+
+import com.ibm.websphere.crypto.PasswordUtil;
+import com.ibm.ws.common.encoder.Base64Coder;
+
+public class CryptoType {
+    
+    public static void main(String[] args) {
+        System.out.println("Password Type Detection Utility");
+        
+        if (args.length != 1) {
+            System.out.println("Usage: java crypoType <password_hash>");
+            System.out.println("Example: java crypoType {xor}Lz4sLCgwLTs=");
+            System.exit(1);
+        }
+        
+        String passwordHash = args[0];
+        System.out.println("Input: " + passwordHash);
+        String detectedType = null;
+        
+        if (PasswordUtil.isValidCryptoAlgorithmTag(passwordHash)) {
+            detectedType = PasswordUtil.getCryptoAlgorithm(passwordHash);
+            System.out.println("Detected Type: " + detectedType);
+        } else {
+            System.err.println("Invalid Crypto Algorithm");
+            System.exit(2);
+        }
+
+        if (detectedType.equalsIgnoreCase("aes")) {
+            String password = PasswordUtil.removeCryptoAlgorithmTag(passwordHash);
+            byte[] passwordBytes = Base64Coder.base64Decode(password.getBytes(StandardCharsets.UTF_8));
+
+            switch (passwordBytes[0]) {
+                case 0: 
+                    System.out.println("AES Encyption type: AES_V0 : AES-128");
+                    break;
+                case 1: 
+                    System.out.println("AES Encyption type: AES_V1 : AES-256");
+                    break;
+                case 2: 
+                    System.out.println("AES Encyption type: AES_V2 : AES-256");
+                    break;
+                default: 
+                    System.err.println("AES Unknown Encryption");
+                    System.exit(3);
+            }
+        }
+    }
+}
